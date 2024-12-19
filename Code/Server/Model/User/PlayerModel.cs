51abc442245347132ea97585dc65572c6f1faf65
Kim Seogyoung <55120754+Kimseogyoung@users.noreply.github.com>
2024-12-20 02:47:23 +0900

    래퍼런스에 맞게 Player Model 수정 및 마이그레이션

--- Code/Server/Model/User/PlayerModel.cs
+++ Code/Server/Model/User/PlayerModel.cs
@@ -9,19 +9,27 @@ namespace WebStudyServer.Model
     public class PlayerModel : ModelBase
     {
         public ulong Id { get; set; } = default;
-
         public ulong AccountId { get; set; } = default;
-
         public ulong SfId { get; set; } = default;
-
         [MaxLength(30)]
         public string ProfileName { get; set; } = "NO_NAME";
-
         public int Lv { get; set; } = DEF.DEFAULT_LV;
-
         public EPlayerState State { get; set; } = default;
-
         public ulong Flag { get; set; } = default;
+        public ulong Exp { get; set; } = default;
+        public int AccExp { get; set; } = default;
+        public int ProfileTitleName { get; set; } = default;
+        public int ProfileIconNum { get; set; } = default;
+        public int ProfileFrameNum { get; set; } = default;
+        public int ProfileCookieNum { get; set; } = default;
+        public ulong GuildId { get; set; } = default;
+        public double KingdomExp { get; set; } = default;
+        public double Gold { get; set; } = default;
+        public double AccGold { get; set; } = default;
+        public double RealCash { get; set; } = default;
+        public double FreeCash { get; set; } = default;
+        public double AccRealCash{ get; set; } = default;
+        public double AccFreeCash { get; set; } = default;
 
     }
 }
